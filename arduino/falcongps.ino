76d2d5f5644d94fa45f5b0b251017201d77176f3
Sergi <acasualcolumbus@gmail.com>
2024-04-07 02:36:50 +0200

    Final push

--- arduino/falcongps.ino
+++ arduino/falcongps.ino
@@ -1,51 +1,45 @@
-//Libraries:
 #include <Wire.h>
-#include <VMA430_GPS.h>     // Include the GPS module library
-#include <SoftwareSerial.h> // Include the software serial library
-SoftwareSerial ss(3, 2); // RX, TX
-VMA430_GPS gps(&ss);     // Pass the softwareserial connection info the the GPS module library
+#include <VMA430_GPS.h>
+#include <SoftwareSerial.h>
+SoftwareSerial ss(3, 2);
+VMA430_GPS gps(&ss);
 
-//Variables and constants:
-const int ID_Machine = 1; //ID Machine of the HYPOTHETICAL device
-const int MPU = 0x68; //I2C address of the MPU-6050
+const int ID_Machine = 1;
+const int MPU = 0x68;
 int16_t GyX,GyY,GyZ;
-int x, y, z;
 
-//Set-up:
 void setup(){
   Wire.begin();
   Wire.beginTransmission(MPU);
-  Wire.write(0x6B); //PWR_MGMT_1 register
-  Wire.write(0); //set to zero (wakes up the MPU-6050)
+  Wire.write(0x6B);
+  Wire.write(0);
   Wire.endTransmission(true);
-  Serial.println("Hello");
-  gps.begin(9600); // Sets up the GPS module to communicate with the Arduino over serial at 9600 baud
-  gps.setUBXNav(); // Enable the UBX mavigation messages to be sent from the GPS module
+  Serial.println();
+  gps.begin(9600);
+  gps.setUBXNav();
   Serial.begin(9600);
 }
 
-//Loop:
 void loop(){
   Wire.beginTransmission(MPU);
-  Wire.write(0x3B); //starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.write(0x3B);
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU,14,true); //request a total of 14 registers
-  GyX = Wire.read()<<8 | Wire.read(); //0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyY = Wire.read()<<8 | Wire.read(); //0x45 (GYRO_YUT_H) & 0x46 (GYRO_YOUT_L)
-  GyZ = Wire.read()<<8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  Wire.requestFrom(MPU,14,true);
+  GyX = Wire.read()<<8 | Wire.read();
+  GyY = Wire.read()<<8 | Wire.read();
+  GyZ = Wire.read()<<8 | Wire.read();
   
-  if(GyX > 0 || GyY > 0 || GyZ > 0){
+  if(GyX < 1000){
     Serial.print(" | GyX = "); Serial.print(GyX);
     Serial.print(" | GyY = "); Serial.print(GyY);
     Serial.print(" | GyZ = "); Serial.println(GyZ);
-  } else if(GyX < 1000 || GyZ < 0){
+  } else if(GyX > 1000 ){
     Serial.println("");
-    Serial.print(" | ID_Machine@"); Serial.println(ID_Machine);Serial.println("@");
+    Serial.print(" | ID_Machine@");
     Serial.println("Locating...");
-    if (gps.getUBX_packet()){ // If a valid GPS UBX data packet is received...
-      gps.parse_ubx_data(); // Parse the new data
-      if (gps.utc_time.valid){ // If the utc_time passed from the GPS is valid...
-        // Print UTC time hh:mm:ss
+    if (gps.getUBX_packet()){
+      gps.parse_ubx_data();
+      if (gps.utc_time.valid){
         Serial.println();
         Serial.print("UTC time: ");
         Serial.print(gps.utc_time.hour);
@@ -55,13 +49,12 @@ void loop(){
         Serial.print(gps.utc_time.second);
         Serial.println();
       }
-    // Print location (latitude/longitude)
     Serial.println();
     Serial.print("location: ");
     Serial.print("Lat: ");
-    Serial.print(gps.location.latitude, 8); // to 8 decimals
+    Serial.print(gps.location.latitude, 8);
     Serial.print(" Long: ");
-    Serial.print(gps.location.longitude, 8); // to 8 decimals
+    Serial.print(gps.location.longitude, 8);
     }
   }
   delay(1000);
--- java/program/falcongps.java
+++ java/program/falcongps.java
@@ -1,5 +1,4 @@
 package falcongps;
-import com.mysql.jdbc.Connection;
 import comunicacionserial.ArduinoExcepcion;
 import comunicacionserial.ComunicacionSerial_Arduino;
 import java.sql.DriverManager;
@@ -12,14 +11,13 @@ import jssc.SerialPortEvent;
 import jssc.SerialPortEventListener;
 import jssc.SerialPortException;
 
-
-public class falcongps extends javax.swing.JFrame {
-    private static Connection con;
+public class FalconGPS {
     private static final String driver = "com.mysql.jdbc.Driver";
     private static final String user = "root";
     private static final String pass = "password";
     private static final String url = "jdbc:mysql://localhost:3306/falcongps?&characterEncoding=latin1&useConfigs=maxPerformance";
     
+    int Id_Machine;
     String sendingId = "";
     String[] Id_Storage;
     int counter = 0;
@@ -30,8 +28,8 @@ public class falcongps extends javax.swing.JFrame {
         public void serialEvent(SerialPortEvent spe) {
             try {
                 if(conection.isMessageAvailable()){
-                    Localizacion.setText(conection.printMessage());
-                    sendingId = Localizacion.getText();
+                    System.out.println("Localizacion: " + conection.printMessage());
+                    sendingId = conection.printMessage();
                     if(sendingId.contains("ID_Machine@") && counter == 0){
                         Id_Storage = sendingId.split("@", 3);
                         System.out.println(Id_Storage[1]);
@@ -43,12 +41,11 @@ public class falcongps extends javax.swing.JFrame {
                 Logger.getLogger(falcongps.class.getName()).log(Level.SEVERE, null, ex);
             } catch (ArduinoExcepcion ex) {
                 Logger.getLogger(falcongps.class.getName()).log(Level.SEVERE, null, ex);
-            };
+            }
         }
     };
     
     public falcongps() {
-        initComponents();
         try {
             conection.arduinoRXTX("COM5", 9600, listen);
         } catch (ArduinoExcepcion ex) {
@@ -57,82 +54,24 @@ public class falcongps extends javax.swing.JFrame {
     }
         
     public void queryFunction(String idStorage){
-        con = null;
-        try{
+        try {
             Class.forName(driver);
-            con = (Connection) DriverManager.getConnection(url, user, pass);
-            if (con != null){
-                StatusMessage.setText("Succesful Connection");
+            try (java.sql.Connection con = DriverManager.getConnection(url, user, pass)) {
+                if (con != null){
+                    System.out.println("Succesful Connection");
+                }
+                Statement consultVariable = con.createStatement();
+                ResultSet rs = consultVariable.executeQuery("SELECT * FROM mainuser WHERE ID_Device = " + idStorage);
+                while (rs.next()){
+                    System.out.println(rs.getString(1) + " " + rs.getString(2));
+                }
             }
-            Statement consultVariable = con.createStatement();
-            ResultSet rs = consultVariable.executeQuery ("SELECT * FROM mainuser WHERE ID_Device = " + idStorage);
-            while (rs.next()){
-                System.out.println (rs.getString(1) + " " + rs.getString(2));
-            }
-            con.close();
         } catch(ClassNotFoundException | SQLException e){
-            StatusMessage.setText("Failed Connection: " + e);
+            System.out.println("Failed Connection: " + e);
         }
-    }
-    @SuppressWarnings("unchecked")
-    
-    private void initComponents() {
-        Localizacion = new javax.swing.JLabel();
-        StatusMessage = new javax.swing.JLabel();
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        Localizacion.setText("Localizacion:");
-        StatusMessage.setText("Status");
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(105, 105, 105)
-                        .addComponent(Localizacion))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(150, 150, 150)
-                        .addComponent(StatusMessage)))
-                .addContainerGap(213, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(134, 134, 134)
-                .addComponent(Localizacion)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 128, Short.MAX_VALUE)
-                .addComponent(StatusMessage)
-                .addContainerGap())
-        );
-        pack();
     }
     
     public static void main(String[] args) {
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(falcongps.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(falcongps.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(falcongps.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(falcongps.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new falcongps().setVisible(true);
-            }
-        });
+        new falcongps();
     }
-    
-    private javax.swing.JLabel Localizacion;
-    private javax.swing.JLabel StatusMessage;
 }
